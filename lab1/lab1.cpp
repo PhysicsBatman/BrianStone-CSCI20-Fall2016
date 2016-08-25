35796977501b04b10a06d680b756ce309c09de50
Brian Stone <bstone006@student.butte.edu>
2016-08-25 20:57:48 +0000

    ASCII Art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,21 @@
+//Brian Stone
+//25 August, 2016
+//ASCII Art (Taste the Rainbow)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"      ______________________     "<<endl;
+    cout<<"     / ____________________ \\      "<<endl;
+    cout<<"    / / __________________ \\ \\     "<<endl;
+    cout<<"   / / / ________________ \\ \\ \\    "<<endl;
+    cout<<"  / / / / ______________ \\ \\ \\ \\   "<<endl;
+    cout<<" / / / / /              \\ \\ \\ \\ \\ "<<endl;
+    cout<<"/ / / / /                \\ \\ \\ \\ \\"<<endl;
+    cout<<"| | | | |                | | | | |"<<endl;
+    cout<<"| | | | |                | | | | |"<<endl;
+    cout<<"| | | | |                | | | | |"<<endl;
+    cout<<"| | | | |                | | | | |"<<endl;
+}
