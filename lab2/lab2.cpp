b2b3c1b8e877dd580c0806962aeaa6df94b96d3e
Brian Stone <bstone006@student.butte.edu>
2016-08-25 21:07:12 +0000

    Guessing Game

---
+++ lab2/lab2.cpp
@@ -0,0 +1,16 @@
+//Correct number = random number between 1 and 10
+//Points = 10
+//Prompt user for guess
+//User guesses a number
+//If (User's guess == Correct number) then
+//      Output "Congratulations! You've won (Points) points!"
+//Else
+//      Points = Points - 1
+//      If (Correct number > User's guess) then
+//          Output "Wrong! You guessed too low!"
+//      Else
+//          Output "Wrong! You guessed too high!"
+//      End
+//      Output "Guess again!"
+//      Return to line 3
+//End
