4946d83f3db39d28a790a1ae65f11e3c3cb39c9e
Brian Stone <bstone006@student.butte.edu>
2016-09-13 20:58:35 +0000

    Lab 6 submission

--- lab6/lab6.cpp
+++ lab6/lab6.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void copyright() {
+    cout << endl;
+    cout << "（ ͡° ͜ʖ ͡°)つ━☆・*。" << endl;
+    cout << "⊂　　 ノ 　　　・゜+." << endl;
+    cout << "　しーＪ　　　°。+ ´¨)" << endl;
+    cout << "　　　　　　　　　.· ´¸.·*´¨) ¸.·*¨)" << endl;
+    cout << "　　　　　　　　　　(¸.·´ (¸.·' ☆ Copyright Brian Stone 2016" << endl;
+}
+
 int main()
 {
     string name;
@@ -23,4 +32,6 @@ int main()
     cout << "Once upon a time, there was a person named " << name << ". " << name << " had " << number1 << " " << noun << ". One of these " << noun << ", however, was very special to " << name << ", because it was inscribed with a single \"" << character << "\". One day, " << name << " realized this most prized possession was missing. And so they traveled " << number2 << " miles, searching for this most sacred of " << noun << "." << endl;
     cout << "They found it." << endl;
     cout << "The End." << endl;
+    
+    copyright();
 }
